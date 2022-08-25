d177f6799bfc4662daa99d258456858cb3ac1bcd
david <davidtucu@hotmail.com>
2022-08-25 07:58:08 -0300

    first commit

---
+++ components/NuevoPresupuesto.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NuevoPresupuesto = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+        <div className="campo">
+            <label>Definir Presupuesto</label>
+            <input
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Añade tu Presupuesto"
+            />
+            <input
+            type="submit" value="Añadir"/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
